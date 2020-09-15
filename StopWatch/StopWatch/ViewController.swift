ecf9e42c06327e9727a47b36ae95f5b8024008ed
Tyler Inari <s1922054@stu.musashino-u.ac.jp>
2020-09-15 17:47:47 +0900

    stopwatch commit

--- StopWatch/StopWatch/ViewController.swift
+++ StopWatch/StopWatch/ViewController.swift
@@ -20,11 +20,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @objc func up() {
+        // countを0.01足す
+        count = count + 0.01
+        // ラベルに小数点以下2桁まで表示
+        label.text = String(format: "%.2f", count)
+    }
+    
     @IBAction func start() {
         
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true
+            )
+        }
     }
     
     @IBAction func stop() {
+        if timer.isValid {
+            // タイマーが動作していたら停止
+            timer.invalidate()
+        }
         
     }
 }
